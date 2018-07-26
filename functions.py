b116a525ec6cecc742db855e790c88b87d5defca
Jooyoon Byun <nooyooj@gmail.com>
2018-07-25 22:51:05 -0400

    .

--- functions.py
+++ functions.py
@@ -23,16 +23,78 @@ def display(data):
 		if r in 'CF': 
 			print(line)
 
-def create_board(gridInString):
+def create_initial_board(gridInString):
+        assert len(gridInString) == 81
+
+        return dict(zip(nodes, gridInString))
+
+def create_board_with_possibilities(gridInString):
         values = []
         digits = '123456789'
 
         for c in gridInString:
                 if c == '.':
-                        values.append(digits)
+                        values.append(digits);
                 elif c in digits:
                         values.append(c)
         
-	assert len(values) == 81
+        assert len(values) == 81
 	
-        return dict(zip(nodes, gridInString))
+        return dict(zip(nodes, values))
+
+def eliminate(values):
+        solved_values = [node for node in values.keys() if len(values[node]) == 1]
+
+        for node in solved_values:
+                digit = values[node]
+
+                for peer in peers[node]:
+                        values[peer] = values[peer].replace(digit, '')
+        
+        return values
+
+def only_choice(values):
+        for unit in unitlist:
+                for digit in '123456789':
+                        dplaces = [node for node in unit if digit in values[node]]
+
+                        if len(dplaces) == 1:
+                                values[dplaces[0]] = digit
+
+        return values
+
+def reduce_puzzle(values):
+        stalled = False
+
+        while not stalled:
+                solved_values_before = len([node for node in values.keys() if len(values[node]) == 1])
+
+                values = eliminate(values)
+                values = only_choice(values)
+
+                solved_values_after = len([node for node in values.keys() if len(values[node]) == 1])
+
+                stalled = solved_values_before == solved_values_after
+
+                if len([node for node in values.keys() if len(values[node]) == 0]):
+                        return False
+        
+        return values
+
+def search(values):
+        values = reduce_puzzle(values)
+
+        if values is False:
+                return False 
+        if all(len(values[s]) == 1 for s in nodes):
+                return values
+        
+        n,s = min((len(values[s]), s) for s in nodes if len(values[s]) > 1)
+
+        for value in values[s]:
+                new_sudoku = values.copy()
+                new_sudoku[s] = value
+                attempt = search(new_sudoku)
+
+                if attempt:
+                        return attempt
--- main.py
+++ main.py
@@ -3,4 +3,18 @@ from functions import *
 gridInString = '7......943..9.217......13856...78.2.5...1...8.2.35...94961......836.5..717......3'
 
 # Displays sudoku board
-display(create_board(gridInString))
+print("\n---Creating initial sudoku board---\n")
+display(create_initial_board(gridInString))
+print("\n---Initial sudoku board successfully created---");
+print ("\n\n")
+
+print("\n---Creating possibilities for each node---\n")
+display(create_board_with_possibilities(gridInString))
+print("\n---Possibilities generated for each empty node---\n")
+print("\n\n")
+
+eliminate(create_board_with_possibilities(gridInString))
+print("\n---Checking for only choices---\n")
+display(only_choice(create_board_with_possibilities(gridInString)))
+print("\n---Only choices generated---\n")
+print("\n\n")
